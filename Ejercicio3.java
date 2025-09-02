585f163476f1eb46840a5eab52f2283760d7bcfe
GFR197 <gfelix.robles02@gmail.com>
2025-09-01 23:41:58 -0700

    Create Ejercicio3.java

---
+++ Ejercicio3.java
@@ -0,0 +1,15 @@
+import static java.lang.System.out;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        int[] Arreglo = {10, 45, 55, 60, 70};
+        out.print("Los Elementos del arreglo son: ");
+
+        for (int i = 0; i < Arreglo.length; i++) {
+            out.print( + Arreglo[i] + " ");
+        }
+
+        
+    }
+}
