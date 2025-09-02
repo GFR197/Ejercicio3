fabd13ae6a431058cb509dd0e2ff574dbe3aebb8
GFR197 <gfelix.robles02@gmail.com>
2025-09-01 23:43:02 -0700

    Create Ejercicio3.js

---
+++ Ejercicio3.js
@@ -0,0 +1,6 @@
+let arreglo = [1, 2, 3, 4, 5]
+let Elementos = "";
+for (let i = 0; i < arreglo.length; i++){
+  Elementos += arreglo[i] + ","
+}
+console.log("Los elementos del arreglo son: " + Elementos)
