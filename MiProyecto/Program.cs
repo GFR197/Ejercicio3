8656af877cea55b4e097220547a31c53430f92f0
GFR197 <gfelix.robles02@gmail.com>
2025-09-01 22:48:10 -0700

    Create Program.cs

---
+++ MiProyecto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System; 
+
+class Program // Definición de la clase principal
+{
+    static void Main(string[] args)
+    {
+        int[] Arreglo = { 100, 200, 300, 400, 500 };
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Writeline($"Los numeros del arreglo son: {Arreglo[i]}");
+
+      }
+
+    }
+}
