cb33e2e7dbc831599ed4e17a40ca9dbbe8dda87a
GFR197 <gfelix.robles02@gmail.com>
2025-09-01 23:44:35 -0700

    Update Program.cs

--- MiProyecto/Program.cs
+++ MiProyecto/Program.cs
@@ -5,12 +5,13 @@ class Program // Definición de la clase principal
     static void Main(string[] args)
     {
         int[] Arreglo = { 100, 200, 300, 400, 500 };
+        Console.Write($"Los numeros del arreglo son: ");
 
         for (int i = 0; i < 5; i++)
         {
-            Console.Writeline($"Los numeros del arreglo son: {Arreglo[i]}");
+            Console.Write(Arreglo[i]);
 
-      }
+        }
 
     }
 }
